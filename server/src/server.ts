65ea772e7c82346d5091539b840e755c492d5d68
rodrigoaugusto99 <rodrigoaugusto51@outlook.com>
2024-01-14 15:35:13 -0300

    Implementação do token JWT

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,11 +2,16 @@ import 'dotenv/config'
 import fastify from 'fastify'
 import { memoriesRoutes } from './routes/memories'
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 
 const app = fastify()
 
 app.register(cors, {
   origin: true
+})
+
+app.register(jwt, {
+  secret: 'spacetime'
 })
 
 app.register(memoriesRoutes)
