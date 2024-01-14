01ff010e29fd2e49e46911f7f885f711bf371ee1
rodrigoaugusto99 <rodrigoaugusto51@outlook.com>
2024-01-14 19:36:04 -0300

    cookies + exibindo perfil do usuario

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,10 +1,15 @@
 import { Copyright } from '@/components/Copyright'
 import { EmptyMemories } from '@/components/EmptyMemories'
 import { Hero } from '@/components/Hero'
+import { Profile } from '@/components/Profile'
 import { SignIn } from '@/components/SignIn'
+import { cookies } from 'next/headers'
 
 
 export default function Home() {
+
+  const isAuthenticated = cookies().has('token')
+  
   return (
     <main className='grid grid-cols-2 min-h-screen'>
       {/*left */}
@@ -12,9 +17,9 @@ export default function Home() {
         {/*blur */}
         <div className="absolute right-0 top-1/2 h-[288px] w-[526px] bg-purple-700 opacity-50 -translate-y-1/2 translate-x-1/2 rounded-full blur-full"/>
         
-        {/*stripes */}
+        {/*estripes */}
         <div className="absolute bottom-0 right-2 top-0 bg-stripes" />
-        <SignIn />
+        {isAuthenticated ? <Profile /> : <SignIn />}
         <Hero/>
         <Copyright />
 
