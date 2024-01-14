f3837b22a716351809ec4471a3e461980d07bcad
rodrigoaugusto99 <rodrigoaugusto51@outlook.com>
2024-01-14 13:12:45 -0300

    Componentização

---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,11 @@
+export function Copyright(){
+    return (
+        <div>
+            Feito com ajuda da <a 
+            className="underline hover:text-gray-50" 
+            target='_blank'
+            rel='noreferrer'
+            href="http://rocketseat.com.br">Rocketseat</a>
+        </div>
+    )
+}
