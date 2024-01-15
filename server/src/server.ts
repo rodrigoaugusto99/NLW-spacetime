cdb8ccd8bcac2ef00aff4214e249d390b9b32de7
rodrigoaugusto99 <rodrigoaugusto51@outlook.com>
2024-01-15 19:26:04 -0300

    lógica de upload de arquivos

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,9 +3,18 @@ import fastify from 'fastify'
 import { memoriesRoutes } from './routes/memories'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
 import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
 
 const app = fastify()
+
+app.register(multipart)
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads'
+})
 
 app.register(cors, {
   origin: true
@@ -15,10 +24,11 @@ app.register(jwt, {
   secret: 'spacetime'
 })
 
+app.register(uploadRoutes)
+
 app.register(memoriesRoutes)
 
 app.register(authRoutes)
-
 
 app
   .listen({
